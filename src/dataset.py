92524caa28194d9fb150f06bd5fb48e294b61f0e
Umang Bansal <ubansal@holylogin07.rc.fas.harvard.edu>
2026-05-11 22:16:49 -0400

    adding datasets

--- src/dataset.py
+++ src/dataset.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 import torch
 from tqdm.auto import tqdm
 from collections import OrderedDict
-
+import json
 
 class ModuleHook:
     def __init__(self, module):
@@ -16,7 +16,10 @@ class ModuleHook:
 
     def hook_fn(self, module, input, output):
         self.module = module
+        if isinstance(output, tuple):
+            output = output[0]
         self.features.append(output.detach())
+        # self.features.append(output.detach())
 
     def close(self):
         self.hook.remove()
@@ -114,7 +117,9 @@ def llama_v2_prompt(
 prompt_translator = {"_age_": "age",
                      "_gender_": "gender",
                      "_socioeco_": "socioeconomic status",
-                     "_education_": "education level",}
+                     "_education_": "education level",
+                     "_priority_": "priority level",
+                    }
 
 
 class TextDataset(Dataset):
@@ -122,7 +127,7 @@ class TextDataset(Dataset):
                  convert_to_llama2_format=False, user_identifier="HUMAN:", ai_identifier="ASSISTANT:", control_probe=False,
                  additional_datas=None, residual_stream=False, new_format=False, if_augmented=False, k=20,
                  remove_last_ai_response=False, include_inst=False, one_hot=False, last_tok_pos=-1):
-        self.file_paths = [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)) and f.endswith('.txt')]
+        self.file_paths = [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)) and f.endswith('.jsonl')]
         self.tokenizer = tokenizer
         self.labels = []
         self.acts = []
@@ -145,35 +150,76 @@ class TextDataset(Dataset):
         self.control_probe = control_probe
         if self.additional_datas:
             for directory in self.additional_datas:
-                self.file_paths += [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)) and f.endswith('.txt')]
+                self.file_paths += [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)) and f.endswith('.jsonl')]
         self._load_in_data()
 
     def __len__(self):
         return len(self.texts)
     
     def _load_in_data(self):
+        corrupted_file_paths = []
         for idx in tqdm(range(len(self.file_paths))):
             file_path = self.file_paths[idx]
-            corrupted_file_paths = []
 
             int_idx = file_path[file_path.find("conversation_")+len("conversation_"):]
             int_idx = int(int_idx[:int_idx.find("_")])
             
             with open(file_path, 'r', encoding='utf-8') as f:
                 text = f.read()
+            try:
+                text = json.loads(text)
+                if isinstance(text, str):
+                    text = json.loads(text)
+            except json.JSONDecodeError:
+                corrupted_file_paths.append(file_path)
+                print(f"Malformed JSON at {file_path}")
+                continue
+            # print(text)
+
+            if (isinstance(text["user_conversation"], list) and len(text["user_conversation"]) > 0):
+                if isinstance(text["user_conversation"][0], dict):
+                    parts = []
+                    for turn in text["user_conversation"]:
+                        if "text" in turn:
+                            parts.append(turn["text"])
+                        elif "assistant" in turn:
+                            parts.append(turn["assistant"])
+                    usr_convo = "\n".join(parts)
+                else:
+                    usr_convo = "\n".join(text["user_conversation"])
+            else: 
+                continue
                 
             if self.convert_to_llama2_format:
-                if "### Human:" in text:
-                    user_msgs, ai_msgs = split_conversation(text, "### Human:", "### Assistant:")
-                elif "### User:" in text:
-                    user_msgs, ai_msgs = split_conversation(text, "### User:", "### Assistant:")
-                else:
-                    user_msgs, ai_msgs = split_conversation(text, self.user_identifier, self.ai_identifier)
                 messages_dict = []
+                if (isinstance(text["user_conversation"][0], dict)):
+                    for line in text["user_conversation"]:
+                        if "text" in line:
+                            if "### Human: " in line['text']:
+                                messages_dict.append({'content': line['text'].replace("### Human:", "").strip() + " ", 'role': 'user'})
+                            elif "### Assistant:" in line['text']: 
+                                messages_dict.append({'content': line['text'].replace("### Assistant:", "").strip() + " ", 'role': 'assistant'})
+                        elif "assistant" in line:
+                            if "### Human: " in line['assistant']:
+                                messages_dict.append({'content': line['assistant'].replace("### Human:", "").strip() + " ", 'role': 'user'})
+                            elif "### Assistant:" in line['assistant']: 
+                                messages_dict.append({'content': line['assistant'].replace("### Assistant:", "").strip() + " ", 'role': 'assistant'})
+                else: 
+                    for line in text["user_conversation"]:
+                        if "### Human:" in line:
+                            messages_dict.append({'content': line.replace("### Human:", "").strip() + " ", 'role': 'user'})
+                        elif "### Assistant:" in line: 
+                            messages_dict.append({'content': line.replace("### Assistant:", "").strip() + " ", 'role': 'assistant'})
+                # if "### Human:" in usr_convo:
+                #     user_msgs, ai_msgs = split_conversation(text, "### Human:", "### Assistant:")
+                # elif "### User:" in usr_convo:
+                #     user_msgs, ai_msgs = split_conversation(text, "### User:", "### Assistant:")
+                # else:
+                #     user_msgs, ai_msgs = split_conversation(text, self.user_identifier, self.ai_identifier)
 
-                for user_msg, ai_msg in zip(user_msgs, ai_msgs):
-                    messages_dict.append({'content': user_msg, 'role': 'user'})
-                    messages_dict.append({'content': ai_msg, 'role': 'assistant'})
+                # for user_msg, ai_msg in zip(user_msgs, ai_msgs):
+                #     messages_dict.append({'content': user_msg, 'role': 'user'})
+                #     messages_dict.append({'content': ai_msg, 'role': 'assistant'})
                     
                 if len(messages_dict) < 1:
                     corrupted_file_paths.append(file_path)
@@ -183,20 +229,23 @@ class TextDataset(Dataset):
                 if self.if_remove_last_ai_response and messages_dict[-1]["role"] == "assistant":
                     messages_dict = messages_dict[:-1]
                 try:
-                    text = llama_v2_prompt(messages_dict) 
-                except:
+                    # print("\n".join(text["developer_instruction"]))
+                    text = llama_v2_prompt(messages_dict, system_prompt="\n".join(text["developer_instruction"]))
+                except Exception as e:
                     corrupted_file_paths.append(file_path)
                     print(f"Corrupted file at {file_path}")
+                    print("Error: ", e)
                     continue
               
             if self.new_format and self.if_remove_last_ai_response and self.include_inst:
-                text = text[text.find("<s>") + len("<s>"):]
+                usr_convo = usr_convo[usr_convo.find("<s>") + len("<s>"):]
             elif self.new_format and self.include_inst:
-                text = text[text.find("<s>") + len("<s>"):]
+                usr_convo = usr_convo[usr_convo.find("<s>") + len("<s>"):]
             elif self.new_format:
-                text = text[text.find("<s>") + len("<s>"): text.rfind("[/INST]") - 1]
+                usr_convo = usr_convo[usr_convo.find("<s>") + len("<s>"): usr_convo.rfind("[/INST]") - 1]
             
-            label = file_path[file_path.rfind(self.label_idf) + len(self.label_idf):file_path.rfind(".txt")]
+            label = file_path[file_path.rfind("_")+1:file_path.rfind(".jsonl")]
+            # print(f"Extracted label: {label!r}, known labels: {list(self.label_to_id.keys())}")
             
             if label not in self.label_to_id.keys():
                 continue
@@ -232,33 +281,37 @@ class TextDataset(Dataset):
                     feature.close()
             
             last_acts = []
+            num_layers = 24
             if self.if_augmented:
+                # num_layers = 41
                 if self.residual_stream:
-                    for layer_num in range(41):
+                    for layer_num in range(num_layers):
                         last_acts.append(output["hidden_states"][layer_num][:, -self.k:].detach().cpu().clone().to(torch.float))
                     last_acts = torch.cat(last_acts, dim=0)
                 else:
                     last_acts.append(features['model.embed_tokens'].features[0][:, -self.k:].detach().cpu().clone().to(torch.float))
-                    for layer_num in range(1, 41):
+                    for layer_num in range(1, num_layers):
                         last_acts.append(features[f'model.layers.{layer_num - 1}.mlp'].features[0][:, -self.k:].detach().cpu().clone().to(torch.float))
                     last_acts = torch.cat(last_acts, dim=0)
             else:
                 if self.residual_stream:
-                    for layer_num in range(41):
+                    for layer_num in range(num_layers):
                         last_acts.append(output["hidden_states"][layer_num][:, -1].detach().cpu().clone().to(torch.float))
                     last_acts = torch.cat(last_acts)
                 else:
                     last_acts.append(features['model.embed_tokens'].features[0][:, -1].detach().cpu().clone().to(torch.float))
-                    for layer_num in range(1, 41):
+                    for layer_num in range(1, num_layers):
                         last_acts.append(features[f'model.layers.{layer_num - 1}.mlp'].features[0][:, -1].detach().cpu().clone().to(torch.float))
                     last_acts = torch.cat(last_acts)
-            
+
             self.texts.append(text)
             self.labels.append(label)
             self.acts.append(last_acts)
             
         for path in corrupted_file_paths:
             self.file_paths.remove(path)
+
+        # print(len(self.texts))
     def __getitem__(self, idx):
         label = self.labels[idx]
         text = self.texts[idx]
